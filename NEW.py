6e6fe71db5487e67a4c9c3a8cffe719c2fbcc53b
natlubim <84182215+natlubim@users.noreply.github.com>
2021-05-16 10:38:36 +0300

    Add files via upload

---
+++ NEW.py
@@ -0,0 +1,13 @@
+m= int(input("введите число от 1 до 12:\n"))
+if m==12 or m==1 or m==2:
+    print ("winter")
+elif m==3 or m==4 or m==5:
+    print ("spring")
+elif m==6 or m==7 or 8 == m:
+    print ("summer")
+elif m==9 or m==10 or m==11:
+    print ("autumn")
+
+
+
+
